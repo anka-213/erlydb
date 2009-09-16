d9fa98cfff240d5f8ac96b42d1f26d94481d3cf3
David Dossot <david@dossot.net>
2009-09-16 15:39:35 -0700

    Added missing util file

--- make-erlydb.sh
+++ make-erlydb.sh
@@ -4,6 +4,7 @@ ERLIB=$(erl -noshell -eval 'io:format([code:lib_dir()]).' -s erlang halt)
 YAWS=$(ls $ERLIB | grep yaws)
 
 cat >Emakefile <<EOF
+{"src/erlyweb/erlyweb_util.erl", [debug_info, {outdir, "ebin"}]}.
 {"src/erlydb/*", [debug_info, {outdir, "ebin"}]}.
 {"src/erlsql/*", [debug_info, {outdir, "ebin"}]}.
 {"src/erltl/*", [debug_info, {outdir, "ebin"}]}.
