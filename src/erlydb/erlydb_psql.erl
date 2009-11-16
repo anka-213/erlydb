74eb42ce3dae1792c9482f8b6d782a40c5304ece
David Dossot <ddossot@pugpharm.com>
2009-11-16 10:30:01 -0800

    Fixed error handling when esql statement contains wrong clause

--- src/erlydb/erlydb_psql.erl
+++ src/erlydb/erlydb_psql.erl
@@ -1,16 +1,16 @@
 %% @author Roberto Saccon (rsaccon@gmail.com)
 %% @copyright Roberto Saccon 2007
-%% 
+%%
 %% @doc This module implements the Postgresql driver for ErlyDB.
 %%
-%% Based on code initially developed by Brian Olson, see 
+%% Based on code initially developed by Brian Olson, see
 %% (http://groups.google.com/group/erlyweb/browse_frm/thread/e1585240f790c87c)
 %%
 %% This is an internal ErlyDB module that you normally shouldn't have to
-%% use directly. 
-%% 
+%% use directly.
+%%
 %% Postgresql driver is an OTP application.
-%% Define database details and authetication credentials for the driver pool 
+%% Define database details and authetication credentials for the driver pool
 %% in psql.app.src and run 'make app' to crate an OTP-ish ebin/psql.app
 %%
 %% For license information see LICENSE.txt
@@ -20,12 +20,12 @@
 -author("Roberto Saccon (rsaccon@gmail.com)").
 
 -export([start/0,
-         stop/0, 
+         stop/0,
          get_metadata/1,
-         q/1, 
-         q/2, 
+         q/1,
+         q/2,
          q2/1,
-         q2/2, 
+         q2/2,
          transaction/2,
          select/2,
          select_as/3,
@@ -47,7 +47,7 @@
 start() ->
     application:load(psql),
     application:start(psql).
-   
+
 
 %% @doc Stops the psql and sql applications.
 stop() ->
@@ -56,22 +56,22 @@ stop() ->
 
 table_names_sql(SchemaName) ->
     "select tablename from pg_tables where schemaname = '" ++
-	SchemaName ++ "'".
-        
+    SchemaName ++ "'".
+
 column_attributes_sql(TableName) ->
     "SELECT a.attname, format_type(a.atttypid, a.atttypmod), d.adsrc, "
-	"a.attnotnull"
-	" FROM pg_attribute a LEFT JOIN pg_attrdef d"
-	" ON a.attrelid = d.adrelid AND a.attnum = d.adnum"
-	" WHERE a.attrelid = '" ++ TableName ++ "'::regclass"
-	" AND a.attnum > 0 AND NOT a.attisdropped"
-	" ORDER BY a.attnum".
+    "a.attnotnull"
+    " FROM pg_attribute a LEFT JOIN pg_attrdef d"
+    " ON a.attrelid = d.adrelid AND a.attnum = d.adnum"
+    " WHERE a.attrelid = '" ++ TableName ++ "'::regclass"
+    " AND a.attnum > 0 AND NOT a.attisdropped"
+    " ORDER BY a.attnum".
 
 constraints_sql(TableName, SchemaName) ->
     "SELECT column_name, constraint_name  FROM"
-	" information_schema.constraint_column_usage where"
-	" table_name = '" ++ TableName ++ "' AND table_schema = '" ++
-	SchemaName ++ "'".
+    " information_schema.constraint_column_usage where"
+    " table_name = '" ++ TableName ++ "' AND table_schema = '" ++
+    SchemaName ++ "'".
 
 %% @doc Get the table names and fields for the database.
 get_metadata(_Options) ->
@@ -79,15 +79,15 @@ get_metadata(_Options) ->
     Schema = "public",
     TableNames = q3(Pid, table_names_sql(Schema)),
     ConstraintInfo =
-	lists:flatten(
-	  [q3(Pid, constraints_sql(element(1, Name), Schema)) ||
-	      Name <- TableNames]),
+    lists:flatten(
+      [q3(Pid, constraints_sql(element(1, Name), Schema)) ||
+          Name <- TableNames]),
     Result = case catch lists:foldl(
-			  fun(Table, TablesTree) ->
-				  get_metadata(Pid, Table, ConstraintInfo,
-					       TablesTree)
-			  end, 
-			  gb_trees:empty(), TableNames) of
+              fun(Table, TablesTree) ->
+                  get_metadata(Pid, Table, ConstraintInfo,
+                           TablesTree)
+              end,
+              gb_trees:empty(), TableNames) of
                  {error, Err} -> exit(Err);
                  Tree -> Tree
              end,
@@ -97,10 +97,10 @@ get_metadata(_Options) ->
 get_metadata(Pid, Table, ConstraintInfo, TablesTree) ->
     Columns = q3(Pid, column_attributes_sql(element(1, Table))),
     Fields = [new_field(Column, element(1, Table), ConstraintInfo) ||
-		 Column <- Columns],
+         Column <- Columns],
     TableName = list_to_atom(element(1, Table)),
     gb_trees:enter(TableName, lists:reverse(Fields), TablesTree).
-                                                          
+
 new_field(FieldInfo, TableName, ConstraintInfo) ->
     Name = element(1, FieldInfo),
     Type = parse_type(element(2, FieldInfo)),
@@ -114,9 +114,9 @@ new_field(FieldInfo, TableName, ConstraintInfo) ->
                              case element(2, Elem) of
                                  CName -> element(1, Elem);
                                  _ -> none
-                             end  
+                             end
                      end,
-                     ConstraintInfo),    
+                     ConstraintInfo),
     Key = case lists:member(Name, Keys) of
               true -> primary;
               _ -> undefined
@@ -137,12 +137,12 @@ parse_type(TypeStr) ->
 
 parse_default([]) ->
     {undefined, undefined};
- 
+
 parse_default(DefaultStr) ->
     case string:str(DefaultStr, "nextval") of
-        0 -> 
-            Default = hd(string:tokens(DefaultStr, "::")),  
-            {Default, undefined};    
+        0 ->
+            Default = hd(string:tokens(DefaultStr, "::")),
+            {Default, undefined};
         _ ->
             {undefined, identity}
     end.
@@ -179,18 +179,20 @@ extra(_X) -> _X.
 %% queries as well as ErlSQL queries with binary and/or string expressions are
 %% accepted. Otherwise the function crashes.
 q(Statement) ->
-    q(Statement, undefined).           
+    q(Statement, undefined).
 
 
 q({esql, Statement}, Options) ->
     Statement2 = fix_statement(Statement),
     case allow_unsafe_statements(Options) of
-        true -> 
+        true ->
             {ok, q2(erlsql:unsafe_sql(Statement2), Options)};
+
         _ ->
-            case catch erlsql:sql(Statement2) of
+            case erlsql:sql(Statement2) of
                 {error, _} = Err ->
                     exit(Err);
+
                 Sql ->
                     {ok, q2(Sql, Options)}
                     %% TODO: catch errors
@@ -201,16 +203,16 @@ q({esql, Statement}, Options) ->
 %% @doc Execute a (binary or string) statement against the Postgresql driver
 %% using the default options.
 %%
-%% @spec q2(Statement::string() | binary()) -> 
+%% @spec q2(Statement::string() | binary()) ->
 %%   psql_result()
 q2(Statement) ->
     q2(Statement, undefined).
-  
-            
+
+
 %% @doc Execute a (binary or string) statement against the PostgreSQL driver.
 %%
 %% @spec q2(Statement::string() | binary(), Options::proplist()) ->
-%%   psql_result()                      
+%%   psql_result()
 q2(Statement, _Options) ->
     Pid = psql:allocate(),
     Result = q3(Pid, Statement),
@@ -220,8 +222,8 @@ q2(Statement, _Options) ->
 
 q3(Pid, Sql) ->
     %%?L(Sql),
-    case psql:sql_query(Pid, Sql) of     
-        {_FieldInfo,[{_Status, Rows}]} ->      
+    case psql:sql_query(Pid, Sql) of
+        {_FieldInfo,[{_Status, Rows}]} ->
             Rows;
         [{Status, _Rows}] ->
             Status;
@@ -285,21 +287,21 @@ get_select_result({ok, _Rows}=Result, undefined) ->
     Result;
 get_select_result({ok, Rows}, FixedVals)->
     Result = lists:foldl(
-	       fun(Fields, Acc) ->
-		       Row = FixedVals ++
-			   lists_to_binaries(tuple_to_list(Fields)),
-		       [list_to_tuple(Row) | Acc]
-	       end, [], Rows),
+           fun(Fields, Acc) ->
+               Row = FixedVals ++
+               lists_to_binaries(tuple_to_list(Fields)),
+               [list_to_tuple(Row) | Acc]
+           end, [], Rows),
     {ok, Result};
-get_select_result(Other, _) -> 
+get_select_result(Other, _) ->
     Other.
 
 lists_to_binaries(Row) ->
     [to_binary(Field) || Field <- Row].
-    
+
 to_binary([]) -> undefined;
 to_binary(Field) when is_list(Field) -> list_to_binary(Field);
-to_binary(Row) -> Row.    
+to_binary(Row) -> Row.
 
 
 %% @doc Execute a INSERT, DELETE or UPDATE statement.
@@ -310,32 +312,32 @@ to_binary(Row) -> Row.
 update(Statement, Options) ->
     R = q(Statement, Options),
     get_update_result(R).
-                         
-get_update_result({ok, <<"INSERT", Rest/binary>>}) -> 
-    {ok, get_update_element(2, Rest)};  
-get_update_result({ok, <<"DELETE", Rest/binary>>}) -> 
-    {ok, get_update_element(1, Rest)};  
-get_update_result({ok, <<"UPDATE", Rest/binary>>}) -> 
+
+get_update_result({ok, <<"INSERT", Rest/binary>>}) ->
+    {ok, get_update_element(2, Rest)};
+get_update_result({ok, <<"DELETE", Rest/binary>>}) ->
     {ok, get_update_element(1, Rest)};
-            
-get_update_result(Other) -> 
+get_update_result({ok, <<"UPDATE", Rest/binary>>}) ->
+    {ok, get_update_element(1, Rest)};
+
+get_update_result(Other) ->
     Other.
 
-get_update_element(N, Bin) -> 
+get_update_element(N, Bin) ->
     {S, <<0>>} = split_binary(Bin, size(Bin)-1),
     R = lists:nth(N, string:tokens(binary_to_list(S), " ")),
-    {AffectedRows, _} = string:to_integer(R), 
+    {AffectedRows, _} = string:to_integer(R),
     AffectedRows.
 
 
 %% @doc Get the id of the last inserted record.
 %%
 %% @spec get_last_insert_id(TableName::atom(), Options::proplist()) -> term()
-get_last_insert_id(Table, Options) -> 
+get_last_insert_id(Table, Options) ->
     Sql = "SELECT currval('" ++ get_sequence_name(Table) ++ "');",
     case q2(Sql, Options) of
         [{N}] ->  {ok, N};
-        Err -> exit(Err)   
+        Err -> exit(Err)
     end.
 
 get_next_insert_id(Table, Options) ->
